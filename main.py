3bfcb4f03f2d3c42aa91d2ee4dcc3515e2076a75
amineallali <amineallali9@gmail.com>
2025-07-17 19:21:32 +0100

    mohawala

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 from scraper import check_new_listings
-from flask import Flask, jsonify
 import time
 import warnings
 import os
 
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 warnings.filterwarnings("ignore")
 
-app = Flask(__name__)
-
-@app.route("/", methods=["GET"])
-def home():
-    try:
-        check_new_listings()
-        time.sleep(30)
-        return jsonify({"status": "success", "message": "Listings checked successfully"}), 200
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    while True:
+        try:
+            check_new_listings()
+        except Exception as e:
+            print(f"Error: {e}")
+        time.sleep(30)
