e58bc8eda640312f866545ad6c24ff8ce0f5305a
hamiiiid02 <155922108+hamiiiid02@users.noreply.github.com>
2025-08-19 14:30:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ from datetime import datetime
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 warnings.filterwarnings("ignore")
 
-if __name__ == "__main__":
-    while True:
-        current_datetime = datetime.now()
-        current_time = current_datetime.strftime("%H:%M:%S")
-        print("bot runned at :", current_time)
-        try:
-            check_new_listings()
-        except Exception as e:
-            print(f"Error: {e}")
-        time.sleep(30)
+print("bot started now !!!")
+
+while True:
+    current_datetime = datetime.now()
+    current_time = current_datetime.strftime("%H:%M:%S")
+    print("bot runned at :", current_time)
+    try:
+        check_new_listings()
+    except Exception as e:
+        print(f"Error: {e}")
+    time.sleep(30)
